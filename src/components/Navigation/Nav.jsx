f84e53a46bf5f7587124da2ad3b27b8bf051e204
vk-develops <vimalkumarv291@gmail.com>
2023-08-06 18:32:37 +0530

    Added necessary package and updated Favicon

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import './Nav.css'
 import Logo from '../../assets/Logo.png'
 import Button from '../Button/Button';
+import { useRef } from 'react';
 
 const Nav = () => {
+
+    const navRef = useRef();
+
+    const showNav = () => {
+        navRef.current.classList.toggle("Responsive-nav");
+    }
+
     return (
         <div className="navii">
             <nav className="navigation">
@@ -18,6 +26,12 @@ const Nav = () => {
                     <Button title="Sign up" />
                 </div>
             </nav>
+            <div className="responsive-nav-elements">
+                    <a className='nav-links nav-active' href="#">Home</a>
+                    <a className='nav-links' href="#">Community</a>
+                    <a className='nav-links' href="#">Features</a>
+                    <Button title="Sign up" />
+                </div>
         </div>
     );
 }
