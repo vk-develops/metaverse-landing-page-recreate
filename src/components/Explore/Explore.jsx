3907d785722d5560757ed0072a84bc12d8e26526
vk-develops <vimalkumarv291@gmail.com>
2023-08-08 20:56:36 +0530

    Fixed explore section bug

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -8,35 +8,35 @@ const Explore = () => {
     return (
         <section className="explore-section">
             <Circle />
-            <div className="explore-left">
-                <img src={ExploreImg} alt="Image" />
-            </div>
             <div className="explore-right">
                 <div className="explore-contents">
                     <div className="explore-main-contents">
-                        <h1 className="heading">Explore Metaverse <br />  Even More</h1>
+                        <h1 className="heading">Explore Metaverse Even More</h1>
                         <p className='paragraph'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or that andomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,.</p>
                     </div>
                     <div className="explore-sub-contents">
-                        <div className="sub-contents">
-                            <h5 className='heading-very-small'>Join Now</h5>
-                            <p className='paragraph-very-small'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
-                            <div className="sub-content-link">
+                        <div className="explore-sub">
+                            <h5 className="heading-very-small">Join Now</h5>
+                            <p className="paragraph-very-small">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
+                            <div className="explore-sub-link">
                                 <a href="#">Explore Now</a>
-                                <AiOutlineArrowRight color="#1D51FE" size={20} />
+                                <AiOutlineArrowRight color='1D51FE' size={24}/>
                             </div>
                         </div>
                         <div className="line"></div>
-                        <div className="sub-contents">
-                            <h5 className='heading-very-small'>Join Now</h5>
-                            <p className='paragraph-very-small'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
-                            <div className="sub-content-link">
+                        <div className="explore-sub">
+                            <h5 className="heading-very-small">Own Your Space</h5>
+                            <p className="paragraph-very-small">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>         
+                            <div className="explore-sub-link">
                                 <a href="#">Explore Now</a>
-                                <AiOutlineArrowRight color="#1D51FE" size={20} />
+                                <AiOutlineArrowRight color='1D51FE' size={24}/>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="explore-left">
+                <img src={ExploreImg} alt="Image" />
             </div>
         </section>
     );
