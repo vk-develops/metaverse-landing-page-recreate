0fa4a0339a301e30dc5fde01b604c2cd06990b8f
vk-develops <vimalkumarv291@gmail.com>
2023-08-09 20:48:07 +0530

    Fixed bug in experienece section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -15,15 +15,15 @@ const Experience = () => {
             </div>
             <div className="experience-contents">
                 <div className="experience-left">
-                    <img src={ExperienceRightGIF} alt="" />
                     <div className="left-texts">
                         <h1 className="heading">Join communities</h1>
                         <p className='paragraph'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from It is a long established fact that a reader will be distracted by</p>
                     </div>
+                    <img src={ExperienceRightGIF} alt="" />
                 </div>
                 <div className="experience-right">
-                    <p className='paragraph'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
                     <img src={ExperienceLeftGIF} alt="" />
+                    <p className='paragraph'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
                 </div>
             </div>
         </section>
