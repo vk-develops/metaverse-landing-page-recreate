eec85b75adb78d387e6b572bf8b11445f93593ed
vk-develops <vimalkumarv291@gmail.com>
2023-08-11 12:05:49 +0530

    ExploreTwo section added

--- src/components/ExporeTwo/ExploreTwo.jsx
+++ src/components/ExporeTwo/ExploreTwo.jsx
@@ -7,21 +7,36 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 const ExploreTwo = () => {
     return (
         <section className="exploretwo-section">
-            <div className="exploretwo-left">
-                <div className="left-contents">
-                    <div className="img-box">
-                        <img src={ExploreTwoLeft} alt="" />
+            <div className="exploretwo">
+                <div className="exploretwo-left">
+                    <div className="exploretwo-contents">
+                        <div className="img-box">
+                            <img src={ExploreTwoLeft} alt="" />
+                        </div>
+                        <div className="exploretwo-texts">
+                            <h1 className="heading-very-small">Mordern and Traditional Costume</h1>
+                            <div className="explore-sub-link">
+                                <a href="#">Explore Now</a>
+                                <AiOutlineArrowRight color='1D51FE' size={24}/>
+                            </div>
+                        </div>
                     </div>
-                    <div className="left-texts">
-                        <h1 className="heading-small">Mordern and Traditional Costume</h1>
-                        <div className="explore-sub-link">
-                            <a href="#">Explore Now</a>
-                            <AiOutlineArrowRight color='1D51FE' size={24}/>
+                </div>
+                <div className="exploretwo-right">
+                    <div className="exploretwo-contents">
+                        <div className="img-box">
+                            <img src={ExploreTwoRight} alt="" />
+                        </div>
+                        <div className="exploretwo-texts">
+                            <h1 className="heading-very-small">Join Over 300+ Groups Communities</h1>
+                            <div className="explore-sub-link">
+                                <a href="#">Explore Now</a>
+                                <AiOutlineArrowRight color='1D51FE' size={24}/>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="exploretwo-right"></div>
         </section>
     );
 }
